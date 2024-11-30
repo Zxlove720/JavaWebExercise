ad26e5dcaa28df1e0f9b06ead36ffc1e786a8b2a
wzb <2482924813@qq.com>
2024-11-30 16:31:29 +0800

    User Mapper

---
+++ JavaWeb05_MyBatis/src/test/java/com/wzb/JavaWeb05MyBatisApplicationTests.java
@@ -0,0 +1,29 @@
+package com.wzb;
+
+import com.wzb.mapper.UserMapper;
+import com.wzb.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+// 这是SpringBoot框架中的单元测试注解，当测试类的测试方法运行时，就会启动SpringBoot项目
+// SpringBoot项目启动了，IOC容器就创建好了，若想测试IOC容器中的bean就可以直接注入了
+class JavaWeb05MyBatisApplicationTests {
+
+    private UserMapper userMapper;
+    @Autowired
+    public JavaWeb05MyBatisApplicationTests(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Test
+    public void testSelectAll() {
+        List<User> userList = userMapper.selectAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+    }
+}
