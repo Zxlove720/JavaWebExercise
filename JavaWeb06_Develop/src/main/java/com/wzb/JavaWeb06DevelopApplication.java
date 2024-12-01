8b8e511dbd98ebebcdb0141255c8a5a7c0e0fc69
wzb <2482924813@qq.com>
2024-12-01 23:07:34 +0800

    SpringBoot yaml configuration

---
+++ JavaWeb06_Develop/src/main/java/com/wzb/JavaWeb06DevelopApplication.java
@@ -0,0 +1,13 @@
+package com.wzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeb06DevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWeb06DevelopApplication.class, args);
+    }
+
+}
