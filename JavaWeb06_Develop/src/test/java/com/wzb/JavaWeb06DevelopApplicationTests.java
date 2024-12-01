02f3b9b4c19e99be1a08b1acc576a950968243c6
wzb <2482924813@qq.com>
2024-12-01 23:13:54 +0800

    test yaml

--- JavaWeb06_Develop/src/test/java/com/wzb/JavaWeb06DevelopApplicationTests.java
+++ JavaWeb06_Develop/src/test/java/com/wzb/JavaWeb06DevelopApplicationTests.java
@@ -1,13 +1,28 @@
 package com.wzb;
 
+import com.wzb.mapper.UserMapper;
+import com.wzb.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class JavaWeb06DevelopApplicationTests {
 
+    private UserMapper userMapper;
+    @Autowired
+    public JavaWeb06DevelopApplicationTests(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     @Test
-    void contextLoads() {
+    public void selectTest() {
+        List<User> userList = userMapper.selectAllUsers();
+        for (User user : userList) {
+            System.out.println(user);
+        }
     }
 
 }
