112e4359e88915c263a20ca2db336da86bd91b16
wzb <2482924813@qq.com>
2025-01-09 14:41:24 +0800

    JUnit测试

--- JavaWeb10_JUnitTest/src/test/java/com/wzb/UserTest.java
+++ JavaWeb10_JUnitTest/src/test/java/com/wzb/UserTest.java
@@ -83,4 +83,69 @@ public class UserTest {
     public static void end() {
         System.out.println("测试完毕");
     }
+
+    @Test
+    @DisplayName("获取年龄：null")
+    public void testGetAgeWithNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, new NamedExecutable() {
+            @Override
+            public void execute() throws Throwable {
+                userService.getAge(null);
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("获取年龄：空串")
+    public void testGetAgeWithEmpty() {
+        Assertions.assertThrows(IllegalArgumentException.class, new NamedExecutable() {
+            @Override
+            public void execute() throws Throwable {
+                userService.getAge("");
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("获取年龄：长度不足")
+    public void testGetAgeWithShort() {
+        Assertions.assertThrows(IllegalArgumentException.class, new NamedExecutable() {
+            @Override
+            public void execute() throws Throwable {
+                userService.getAge("123");
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("获取年龄：长度过长")
+    public void testGetAgeWithLang() {
+        Assertions.assertThrows(IllegalArgumentException.class, new NamedExecutable() {
+            @Override
+            public void execute() throws Throwable {
+                userService.getAge("1000002004101410110000000");
+            }
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100000200410141011"})
+    public void testGetAge1(String idCard) {
+        int age = userService.getAge(idCard);
+        Assertions.assertEquals(20, age);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100000202010141011"})
+    public void testGetAge2(String idCard) {
+        int age = userService.getAge(idCard);
+        Assertions.assertEquals(4, age);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100000199810141011"})
+    public void testGetAge3(String idCard) {
+        int age = userService.getAge(idCard);
+        Assertions.assertEquals(26, age);
+    }
 }
