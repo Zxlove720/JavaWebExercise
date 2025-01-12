10683ef383daeadd2ff3a864d999087877c126ff
wzb <2482924813@qq.com>
2025-01-12 18:02:45 +0800

    DQL练习

--- JavaWeb04_Database/src/test/java/com/wzb/JavaWeb04DatabaseApplicationTests.java
+++ JavaWeb04_Database/src/test/java/com/wzb/JavaWeb04DatabaseApplicationTests.java
@@ -11,24 +11,27 @@ import java.sql.ResultSet;
 
 @SpringBootTest
 class JavaWeb04DatabaseApplicationTests {
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private final String url = "jdbc:mysql://localhost:3306/db01";
+    private final String username = "root";
+    private final String password = "123456";
+
+
     /**
      * 使用JDBC编写DQL语句
      */
     @Test
     public void testDQL() throws Exception {
         // 注册数据库驱动
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName(driver);
         // 创建连接
-        String url = "jdbc:mysql://localhost:3306/db01";
-        String username = "root";
-        String password = "123456";
         Connection connection = DriverManager.getConnection(url, username, password);
         // 编写SQL语句
         String sql = "select id, username, password, name, age from user where username = ? and password = ?";
         // 使用PreparedStatement对象创建预编译SQL
         PreparedStatement statement = connection.prepareStatement(sql);
         // 预编译SQL的两个参数都是String类型的，所以说使用setString方法补全其参数
-        statement.setString(1, "zhouyu");
+        statement.setString(1, "liubei");
         statement.setString(2, "123456");
         // 调用executeQuery方法发起一条DQL语句，并将查询结果封装到ResultSet类中
         ResultSet resultSet = statement.executeQuery();
@@ -45,6 +48,38 @@ class JavaWeb04DatabaseApplicationTests {
         }
         // 释放资源
         resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
+    @Test
+    public void testJDQL() throws Exception {
+        // 注册驱动
+        Class.forName(driver);
+        // 创建数据库连接
+        Connection connection = DriverManager.getConnection(url, username, password);
+        // 编写预编译SQL
+        String sql = "select id, username, password, name, age from user where username = ? and password = ?";
+        // 使用PreparedStatement创建预编译SQL
+        PreparedStatement statement = connection.prepareStatement(sql);
+        // 补全预编译SQL中的参数
+        statement.setString(1, "liubei");
+        statement.setString(2, "123456");
+        // 查询，查询结果封装到ResultSet中
+        ResultSet resultSet = statement.executeQuery();
+        // 遍历ResultSet集合，获取查询数据封装为实体对象
+        while (resultSet.next()) {
+            User user = new User(
+                    resultSet.getInt("id"),
+                    resultSet.getString("username"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getInt("age")
+            );
+            System.out.println(user);
+        }
+        // 释放资源，先开后关
+        resultSet.close();;
         statement.close();
         connection.close();
     }
