f2d7951f10db8ac294967e44563ba999f815659a
wzb <2482924813@qq.com>
2025-01-16 11:16:39 +0800

    部门管理：查询所有部门（开启驼峰命名映射）

--- springboot_emp/src/main/java/com/wzb/controller/DeptController.java
+++ springboot_emp/src/main/java/com/wzb/controller/DeptController.java
@@ -4,8 +4,6 @@ import com.wzb.pojo.Dept;
 import com.wzb.pojo.Result;
 import com.wzb.service.DeptService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,7 +23,7 @@ public class DeptController {
      */
     // 限定请求方式为GET
     // @RequestMapping(value = "/dept", method = RequestMethod.GET)
-    @GetMapping("/dept")
+    @GetMapping("/dept") // 建议使用衍生注解，更加简洁、优雅
     public Result<List<Dept>> selectAllDept() {
         List<Dept> deptList = deptService.selectAllDept();
         return Result.success(deptList);
