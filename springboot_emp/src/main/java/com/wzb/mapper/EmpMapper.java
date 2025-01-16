f2509d1862237e6db65000d47ab6c98e575bf35a
wzb <2482924813@qq.com>
2025-01-16 16:12:02 +0800

    员工管理：使用PageHelper插件进行分页查询查询员工信息

--- springboot_emp/src/main/java/com/wzb/mapper/EmpMapper.java
+++ springboot_emp/src/main/java/com/wzb/mapper/EmpMapper.java
@@ -9,10 +9,28 @@ import java.util.List;
 @Mapper
 public interface EmpMapper {
 
+    /**
+     * 统计员工数量，便于分页查询展示记录数
+     * 但是使用PageHelper插件进行分页查询时，无需该方法
+     * @return 员工总数
+     */
     @Select("select count(*) from emp")
     Long countEmp();
 
-    @Select("select * from emp as e left join dept as d on e.dept_id = d.id limit #{start}, #{pageSize}")
-    List<Emp> selectAllEmp(Integer start, Integer pageSize);
+//    /**
+//     * 使用原始的方式分页查询员工信息
+//     * @param start 分页查询的起始记录
+//     * @param pageSize 每一页最多记录数
+//     * @return List<Emp>
+//     */
+//    @Select("select * from emp as e left join dept as d on e.dept_id = d.id limit #{start}, #{pageSize}")
+//    List<Emp> selectAllEmp(Integer start, Integer pageSize);
+
+    /**
+     * 使用PageHelper插件分页查询员工信息
+     * 使用PageHelper插件时，无需在Mapper接口中提供起始记录start 和 pageSize等参数
+     */
+    @Select("select * from emp as e left join dept as d on e.dept_id = d.id")
+    List<Emp> selectAllEmp();
 
 }
--- springboot_emp/src/main/java/com/wzb/service/impl/EmpServiceImpl.java
+++ springboot_emp/src/main/java/com/wzb/service/impl/EmpServiceImpl.java
@@ -1,5 +1,7 @@
 package com.wzb.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.wzb.mapper.EmpMapper;
 import com.wzb.pojo.Emp;
 import com.wzb.pojo.PageResult;
@@ -22,24 +24,45 @@ public class EmpServiceImpl implements EmpService {
         this.empMapper = empMapper;
     }
 
+//    /**
+//     * 分页查询所有员工信息
+//     * @param page 当前页数
+//     * @param pageSize 每一页最大记录数
+//     * @return PageResult<Emp>
+//     */
+//    @Override
+//    public PageResult<Emp> selectAllEmp(Integer page, Integer pageSize) {
+//        // 获取总记录数
+//        Long total = empMapper.countEmp();
+//
+//        // 获取分页查询的起始记录
+//        // 比如说我想要从第5页开始查，每一页最多10条记录，那么起始记录就是40（（5 - 1） * 10）
+//        Integer start = (page - 1) * pageSize;
+//        // 获取分页查询结果
+//        List<Emp> empList = empMapper.selectAllEmp(start, pageSize);
+//
+//        // 封装结果并返回
+//        return new PageResult<>(total, empList);
+//    }
+
     /**
-     * 分页查询所有员工信息
-     * @param page 当前页数
-     * @param pageSize 每一页最大记录数
+     * 使用PageHelper插件进行员工信息分页查询
+     * @param page 起始页码
+     * @param pageSize 每页最多记录数
      * @return PageResult<Emp>
      */
     @Override
     public PageResult<Emp> selectAllEmp(Integer page, Integer pageSize) {
-        // 获取总记录数
-        Long total = empMapper.countEmp();
-
-        // 获取分页查询的起始记录
-        // 比如说我想要从第5页开始查，每一页最多10条记录，那么起始记录就是40（（5 - 1） * 10）
-        Integer start = (page - 1) * pageSize;
-        // 获取分页查询结果
-        List<Emp> empList = empMapper.selectAllEmp(start, pageSize);
-
-        // 封装结果并返回
-        return new PageResult<>(total, empList);
+        // 使用PageHelper分页插件
+        // 设置分页参数
+        PageHelper.startPage(page, pageSize);
+        // 查询
+        List<Emp> empList = empMapper.selectAllEmp();
+        // 转换查询结果
+        Page<Emp> p = (Page<Emp>) empList;
+        // 封装查询结果到PageResult对象中
+        // 通过getTotal方法获取总记录数
+        // 通过getResult方法获取分页查询结果
+        return new PageResult<>(p.getTotal(), p.getResult());
     }
 }
