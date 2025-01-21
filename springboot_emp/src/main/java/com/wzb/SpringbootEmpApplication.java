ef7266f2855834ffb3b32bd40dc98d8f99e3cf75
wzb <2482924813@qq.com>
2025-01-21 09:28:43 +0800

    统一拦截：过滤器：拦截一切请求

--- springboot_emp/src/main/java/com/wzb/SpringbootEmpApplication.java
+++ springboot_emp/src/main/java/com/wzb/SpringbootEmpApplication.java
@@ -2,8 +2,10 @@ package com.wzb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // Filter属于Servlet组件，spring框架需要使用该注解开启Servlet组件支持
 public class SpringbootEmpApplication {
 
     public static void main(String[] args) {
---
+++ springboot_emp/src/main/java/com/wzb/filter/DemoFilter.java
@@ -0,0 +1,26 @@
+package com.wzb.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*") // 配置Filter过滤器的拦截请求路径，/*表示拦截所有
+public class DemoFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("Init Filter");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("Filter 成功拦截请求");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Destroy Filter");
+    }
+}
