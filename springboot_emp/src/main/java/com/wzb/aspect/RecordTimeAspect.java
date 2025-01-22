b990d6da718cb8d3221b8a9651196cc45a26806e
wzb <2482924813@qq.com>
2025-01-22 22:40:43 +0800

    AOP：AOP初步，统计方法执行用时

---
+++ springboot_emp/src/main/java/com/wzb/aspect/RecordTimeAspect.java
@@ -0,0 +1,29 @@
+package com.wzb.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 通过AOP统计员工操作的耗时
+ */
+@Component
+@Aspect // 将其标记为切面类
+@Slf4j
+public class RecordTimeAspect {
+
+    @Around("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        // 记录方法开始时间
+        Long begin = System.currentTimeMillis();
+        // 调用原始方法
+        Object result = pjp.proceed();
+        // 记录方法结束时间
+        Long end = System.currentTimeMillis();
+        // 计算方法耗时
+        log.info("执行方法耗时：{}", end - begin);
+        return result;
+    }
+}
