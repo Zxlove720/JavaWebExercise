5d29aa3e520980e0b9456d613741395535ace4b1
wzb <2482924813@qq.com>
2025-01-23 09:59:29 +0800

    AOP：抽取切入点表达式到切入点方法中

--- springboot_emp/src/main/java/com/wzb/aspect/MyAspect1.java
+++ springboot_emp/src/main/java/com/wzb/aspect/MyAspect1.java
@@ -15,21 +15,29 @@ import org.springframework.stereotype.Component;
 public class MyAspect1 {
 
     /**
-     * Before 前置通知，在方法运行前执行
-     * @param joinPoint 切入点
+     * 抽取切入点表达式
+     * 抽取到切入点方法中
      */
-    @Before("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    @Pointcut("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    private void pt() {}
+
+
+    /**
+     * Before 前置通知，在方法运行前执行
+     * @param joinPoint 连接点
+     */
+    @Before("pt()")
     public void before(JoinPoint joinPoint) {
         log.info("before 前置通知");
     }
 
     /**
      * Around 环绕通知，在方法运行前后都会执行
-     * @param pjp 切入点
+     * @param pjp 连接点
      * @return 方法返回值
      * @throws Throwable 原始方法运行时抛出异常
      */
-    @Around("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    @Around("pt()")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         log.info("before around");
         // 调用目标对象的原始方法执行
@@ -41,9 +49,9 @@ public class MyAspect1 {
 
     /**
      * After 后置通知方法运行后执行
-     * @param joinPoint 切入点
+     * @param joinPoint 连接点
      */
-    @After("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    @After("pt()")
     public void after(JoinPoint joinPoint) {
         log.info("after 后置通知");
     }
@@ -51,18 +59,18 @@ public class MyAspect1 {
 
     /**
      * 返回后通知 目标方法必须正常运行才会执行返回后通知
-     * @param joinPoint 切入点
+     * @param joinPoint 连接点
      */
-    @AfterReturning("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    @AfterReturning("pt()")
     public void afterReturning(JoinPoint joinPoint) {
         log.info("afterReturning 返回后通知");
     }
 
     /**
      * 异常通知 目标方法必须抛出异常，执行出错才会执行异常通知，异常通知和返回后通知互斥
-     * @param joinPoint 切入点
+     * @param joinPoint 连接点
      */
-    @AfterThrowing("execution(* com.wzb.service.impl.EmpServiceImpl.*(..))")
+    @AfterThrowing("pt()")
     public void afterThrowing(JoinPoint joinPoint) {
         log.info("afterThrowing 异常通知");
     }
