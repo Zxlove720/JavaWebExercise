026b8cdbec10a6a4a1d8b4893ba5d7a49cc2d9ea
wzb <2482924813@qq.com>
2025-02-09 22:52:21 +0800

    redis:jedis：String数据类型

--- redis_study/src/test/java/com/wzb/redis_study/redisTest.java
+++ redis_study/src/test/java/com/wzb/redis_study/redisTest.java
@@ -1,10 +1,13 @@
 package com.wzb.redis_study;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import redis.clients.jedis.Jedis;
 
 public class redisTest {
+
+    private Jedis jedis;
 
     /**
      * redis建立连接
@@ -12,11 +15,24 @@ public class redisTest {
     @BeforeEach
     void setUp() {
         // 建立连接
-        Jedis jedis = new Jedis("192.168.181.129", 6379);
+        jedis = new Jedis("192.168.181.129", 6379);
         // 设置密码
         jedis.auth("123456");
         // 选择库（默认0）
         jedis.select(0);
+    }
+
+    /**
+     * 测试String数据类型
+     */
+    @Test
+    void testString() {
+        // 存
+        String result = jedis.set("name", "张三");
+        System.out.println(result);
+        // 取
+        String name = jedis.get("name");
+        System.out.println(name);
     }
 
 }
