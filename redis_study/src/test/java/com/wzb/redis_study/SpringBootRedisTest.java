6f3882638b5881ee26fde8266872658c72165967
wzb <2482924813@qq.com>
2025-02-11 22:53:46 +0800

    springData：String类型存入对象

--- redis_study/src/test/java/com/wzb/redis_study/SpringBootRedisTest.java
+++ redis_study/src/test/java/com/wzb/redis_study/SpringBootRedisTest.java
@@ -1,5 +1,6 @@
 package com.wzb.redis_study;
 
+import com.wzb.redis_study.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,5 +25,14 @@ public class SpringBootRedisTest {
         // 获取String数据
         Object o = redisTemplate.opsForValue().get("name");
         System.out.println(o);
+    }
+
+    @Test
+    void testObject() {
+        // 写入一条String数据，value是对象
+        User user = new User("虎哥", 18);
+        redisTemplate.opsForValue().set("user", user);
+        // 获取数据
+        System.out.println(redisTemplate.opsForValue().get("user"));
     }
 }
