95c8b7bfa4dfbe20d39f257b802ffc2f7e237ed5
wzb <2482924813@qq.com>
2025-05-28 22:30:05 +0800

    MyBatisPlus：快速入门

---
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/JavaWeb13MyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.wzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeb13MyBatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWeb13MyBatisPlusApplication.class, args);
+    }
+
+}
