8031c78bfb383087cd2579c257dfa8719af187b5
wzb <2482924813@qq.com>
2025-05-30 23:28:23 +0800

    MyBatisPlus：快速入门

--- JavaWeb13_MyBatisPlus/src/main/java/com/wzb/mapper/EmpMapper.java
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/mapper/EmpMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wzb.pojo.Emp;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 
+ */
 @Mapper
 public interface EmpMapper extends BaseMapper<Emp> {
 }
