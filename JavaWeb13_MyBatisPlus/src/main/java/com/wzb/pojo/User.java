4ba990ca93a05f1cf0f96c7ce7594ddabd341edf
wzb <2482924813@qq.com>
2025-06-03 11:43:15 +0800

    MyBatisPlus：快速入门

--- JavaWeb13_MyBatisPlus/src/main/java/com/wzb/pojo/User.java
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/pojo/User.java
@@ -1,6 +1,30 @@
 package com.wzb.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * User实体类
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    private int id;
 
+    private String username;
+
+    private String password;
+
+    private String phone;
+
+    private int balance;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
 }
