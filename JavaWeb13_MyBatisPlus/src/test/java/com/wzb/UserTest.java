f23f8aabf1a29847160805f1728798973c91b6d5
wzb <2482924813@qq.com>
2025-06-03 11:46:14 +0800

    MyBatisPlus：快速入门

--- JavaWeb13_MyBatisPlus/src/test/java/com/wzb/UserTest.java
+++ JavaWeb13_MyBatisPlus/src/test/java/com/wzb/UserTest.java
@@ -1,9 +1,28 @@
 package com.wzb;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UserTest {
 
+    @Test
+    public void testQuery() {
 
+    }
+
+    @Test
+    public void testInsert() {
+
+    }
+
+    @Test
+    public void testDelete() {
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+    }
 }
