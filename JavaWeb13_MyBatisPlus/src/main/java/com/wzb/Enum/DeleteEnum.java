390270f8d22934692071402c839f2a6d8363c29c
wzb <2482924813@qq.com>
2025-06-04 20:24:40 +0800

    MyBatisPlus：枚举处理器

--- JavaWeb13_MyBatisPlus/src/main/java/com/wzb/Enum/DeleteEnum.java
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/Enum/DeleteEnum.java
@@ -1,6 +1,7 @@
 package com.wzb.Enum;
 
 import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,13 +9,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum DeleteEnum {
 
-    NOT_DELETE(0, "未删除"),
-    DELETE(1, "删除"),
+    NOT_DELETE(0, "可用"),
+    DELETE(1, "不可用"),
     ;
 
     // 使用@EnumValue注解标记枚举中的哪一个值需要存入数据库
     @EnumValue
     private final Integer value;
-
+    // 使用@JsonValue注解标记返回数据时，使用枚举中的哪一个值
+    @JsonValue
     private final String desc;
 }
