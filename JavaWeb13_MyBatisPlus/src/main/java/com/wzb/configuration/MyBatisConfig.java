31fab5dc2831b890e21cb7f816b0a0cddc5ab2c2
wzb <2482924813@qq.com>
2025-06-04 20:50:36 +0800

    MyBatisPlus：分页插件

---
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/configuration/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.wzb.configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        // 1.初始化核心插件
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 2.添加分页插件
+        PaginationInnerInterceptor pageInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 3.设置分页插件
+        // 3.1设置分页上限
+        pageInterceptor.setMaxLimit(1000L);
+        // 3.2添加分页插件
+        interceptor.addInnerInterceptor(pageInterceptor);
+        return interceptor;
+    }
+}
