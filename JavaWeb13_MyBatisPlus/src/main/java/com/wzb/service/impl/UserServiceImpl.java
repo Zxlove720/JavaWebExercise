20064b2fcb8120fd2a06d7df06c9e960552069c4
wzb <2482924813@qq.com>
2025-06-04 10:51:16 +0800

    MyBatisPlus：核心功能

--- JavaWeb13_MyBatisPlus/src/main/java/com/wzb/service/impl/UserServiceImpl.java
+++ JavaWeb13_MyBatisPlus/src/main/java/com/wzb/service/impl/UserServiceImpl.java
@@ -1,19 +1,33 @@
 package com.wzb.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wzb.mapper.UserMapper;
 import com.wzb.pojo.User;
 import com.wzb.service.UserService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     @Override
     public void updateBalance(Integer id, Integer amount) {
-
+        User user = getById(id);
+        if (user.getBalance() < amount) {
+            System.out.println("余额不足");
+            throw new RuntimeException("余额不足");
+        }
+        LambdaUpdateWrapper<User> updateWrapper = new LambdaUpdateWrapper<User>()
+                .eq(User::getId, id);
+        userMapper.updateBalance(updateWrapper, amount);
     }
 
 }
