dc467ce036f48bcb52cacfa78620a537c9166e99
wzb <2482924813@qq.com>
2025-07-03 23:18:03 +0800

    Redis：SpringDataRedis

---
+++ redis_springData/src/main/java/com/wzb/RedisSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.wzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringDataApplication.class, args);
+    }
+
+}
