28dce6446f273b021948714d74ac2bd587d5c0e3
wzb <2482924813@qq.com>
2025-07-03 23:32:32 +0800

    Redis：SpringDataRedis

--- redis_springData/src/test/java/com/wzb/RedisSpringDataApplicationTests.java
+++ redis_springData/src/test/java/com/wzb/RedisSpringDataApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wzb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class RedisSpringDataApplicationTests {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Test
-    void contextLoads() {
+    void testString() {
+        // 存储一条String数据
+        redisTemplate.opsForValue().set("name", "jack");
+        // 获取String数据
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 
 }
